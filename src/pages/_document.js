54b62c82aba1a1172e57afd36722dcb5384f18b9
John Dimm <john.dimm@gardyn.tech>
2025-07-03 10:02:24 -0700

    pinch 14

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=1024, initial-scale=1.0, user-scalable=yes, minimum-scale=0.3, maximum-scale=3.0" />
+        <meta name="viewport" content="width=1024, initial-scale=1.0, user-scalable=yes, minimum-scale=0.2, maximum-scale=3.0" />
       </Head>
       <body>
         <Main />
